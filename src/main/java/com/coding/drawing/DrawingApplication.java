db697871b2917b730c09d908b19a0a4170c52130
João Monteiro <monteiro_vsc@hotmail.com>
2021-12-19 20:59:46 +0000

    added input scanner

--- src/main/java/com/coding/drawing/DrawingApplication.java
+++ src/main/java/com/coding/drawing/DrawingApplication.java
@@ -1,9 +1,21 @@
 package com.coding.drawing;
 
+import java.util.Scanner;
 
 public class DrawingApplication {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("enter command: ");
+		while(sc.hasNextLine()){
+			String command = getCommand(sc);
+			if("Q".equals(command)) break;
 
+		}
+	}
+
+	private static String getCommand(Scanner scanner) {
+		System.out.print("enter command: ");
+		return scanner.nextLine();
 	}
 }
