57fde22f4a4278236772e9f02ace267db66ba141
João Monteiro <monteiro_vsc@hotmail.com>
2021-12-19 18:46:44 +0000

    init project

---
+++ src/main/java/com/coding/drawing/DrawingApplication.java
@@ -0,0 +1,9 @@
+package com.coding.drawing;
+
+
+public class DrawingApplication {
+
+	public static void main(String[] args) {
+
+	}
+}
---
+++ src/test/java/com/coding/drawing/DrawingApplicationTests.java
@@ -0,0 +1,12 @@
+package com.coding.drawing;
+
+import org.junit.Test;
+
+public class DrawingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+
+	}
+
+}
